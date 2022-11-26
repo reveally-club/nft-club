d56351714f449d02c8c8904b59bf605e72cf275a
dilrong <dilrong@dilrong.com>
2022-11-27 02:17:17 +0900

    feat: sidebar description

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,15 +9,25 @@ const Layout: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
           <h2 className="text-transparent text-sm bg-clip-text bg-gradient-to-r from-sky-400 to-violet-400">
             with Reveally
           </h2>
-          <p className="mt-4">NFT.club is a list of NFT infomation.</p>
+          <h3 className="mt-2 font-bold text-xl">
+            All the NFT projects in one place
+          </h3>
+          <p className="mt-4 font-sm max-w-sm">
+            NFT.club is a list of NFT projects. <br />
+            Add your NFT projects in a minute! <br />
+            You can connect to each project, can be connected to all the
+            projects you want to check with NFT.club
+          </p>
           <div className="mt-8">
             <button className="text-white p-4 bg-gradient-to-r from-sky-400 to-violet-400 rounded-full hover:text-zinc-100 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
-              Add Yout NFT Project
+              Add Your NFT Project
             </button>
           </div>
         </div>
       </div>
-      <main className="main-content flex flex-col flex-grow p-4 ml-4 container mx-auto">{children}</main>
+      <main className="main-content flex flex-col flex-grow p-4 ml-4 container mx-auto">
+        {children}
+      </main>
     </div>
   );
 };
