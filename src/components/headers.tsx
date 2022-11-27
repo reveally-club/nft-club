7d490ba31586ba517df52e91d6aaed4289a91d27
dilrong <dilrong@dilrong.com>
2022-11-27 16:31:03 +0900

    feat: og

--- src/components/headers.tsx
+++ src/components/headers.tsx
@@ -1,9 +1,17 @@
+import Head from "next/head";
 import ConnectWallet from "./connect-wallet";
 import Search from "./search";
 
 const Headers = () => {
   return (
     <div className="">
+      <Head>
+        <title>NFT.Club with Reveally</title>
+        <meta
+          property="og:description"
+          content="All the NFT projects in one place"
+        />
+      </Head>
       <Search />
       {/* <ConnectWallet /> */}
     </div>
